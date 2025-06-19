82d72bd6559c4715caa29c8ecc8eb1cda045d947
Sai Krishna Nair <saikrishna.nair2412@gmail.com>
2025-06-19 06:34:05 +0530

    fixed docQnA and App.py

--- News.py
+++ News.py
@@ -1,6 +1,7 @@
 import os
 from langchain_core.tools import tool
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 from dotenv import load_dotenv
 from pprint import pprint
 
@@ -9,54 +10,17 @@ load_dotenv()
 # Initialize Tavily client
 tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
 
-@tool
 def financial_news_search(query: str) -> str:
     """
-    Use this tool for general knowledge questions, explanations, how-to questions,
-    or any query that doesn't require specific documents or current information
-    Search for live financial news, market updates, stock prices, economic data, 
-    and financial analysis. Use this tool when users ask about:
-    - Current stock prices or market performance
-    - Breaking financial news
-    - Economic indicators and reports
-    - Company earnings or financial results
-    - Market trends and analysis
-    - Cryptocurrency prices and news
-    - Financial regulatory updates
-    
-    Args:
-        query: The financial topic or question to search for
+    this tool is used to answer queries which needs latest news feed
     """
-
     print("news tool invoked")
     try:
-        print(f"Searching financial news for: {query}")
-        enhanced_query = f"latest financial news on {query}"
-        response = tavily_client.search(
-            query=enhanced_query,
-            topic="finance",
-            time_range="month",
-            max_results=1,
-            country="India",
-            include_domains=[
-                "coindesk.com"
-                "financialexpress.com"
-                "economictimes.indiatimes.com",
-                "livemint.com",
-                "thehindu.com",
-                "wionews.com",
-
-            ],
-            exclude_domains=[
-                "reddit.com",
-                "twitter.com",
-                "facebook.com",
-                "X.com",
-                "twitter.com",
-                "instagram.com"
-            ]
+        tool = TavilySearch(
+            max_results=5,
+            topic="finance"
         )
-        
+        response = tool.invoke(query)
         if not response.get('results'):
             return f"No recent financial news found for: {query}"
         
