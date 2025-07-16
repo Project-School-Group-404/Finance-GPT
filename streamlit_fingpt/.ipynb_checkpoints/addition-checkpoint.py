4f2107c8be388df88978674ceb21ae398fed0770
Sai Krishna Nair <saikrishna.nair2412@gmail.com>
2025-07-16 09:44:55 +0530

    removed useless shit

--- streamlit_fingpt/.ipynb_checkpoints/addition-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def add(a: int, b: int) -> int:
-    """Adds a and b.
-
-    Args:
-        a: first int
-        b: second int
-    """
-    return a + b
--- streamlit_fingpt/.ipynb_checkpoints/multiplication-checkpoint.py
+++
@@ -1,8 +0,0 @@
-def multiply(a: int, b: int) -> int:
-    """Multiply a and b.
-
-    Args:
-        a: first int
-        b: second int
-    """
-    return a * b
